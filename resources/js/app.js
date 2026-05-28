34207f28aaf26a194ebff176cec77b953777b83c
Sean Kim <shskim7@gmail.com>
2026-05-28 10:39:13 +0900

    Default theme mode set as Light. Filament JS updated.

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,10 @@
 import jQuery from 'jquery';
 
 window.$ = window.jQuery = jQuery;
+ 
+// Theme - Dark mode
+if (localStorage.theme === 'dark') {    // || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark');
+} else {
+    document.documentElement.classList.remove('dark');
+}
